e1c42316ec59c428f019738035bb5a7296c1d132
ibrohim-valixanov-web <ibrohimvalixanov778@gmail.com>
2026-07-07 10:19:22 +0500

    newsvg

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) {
+              return 'firebase';
+            }
+            if (id.includes('react-icons')) {
+              return 'react-icons';
+            }
+            if (id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'react-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
