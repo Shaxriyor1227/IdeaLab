0ae25a94f8dc1fd8b3333b1320ea63c58d300ccd
Shaxriyor1227 <kiyotasakiayanakoji@gmail.com>
2026-07-23 20:17:01 +0500

    "Firebase va Vercel sozlamalari yakunlandi"

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbkuFeparyTNCjVshKWFapZ3mbOelzYqc",
-  authDomain: "idealab-a960d.firebaseapp.com",
-  projectId: "idealab-a960d",
-  storageBucket: "idealab-a960d.firebasestorage.app",
-  messagingSenderId: "608971817676",
-  appId: "1:608971817676:web:82aeaf6cb49e88e3983869",
-  measurementId: "G-RBLM2FXHYB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
